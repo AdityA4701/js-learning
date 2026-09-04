4573bd46b219e27fc32e6115e0b2607e82ed928b
Aditya Ranjan <63051958+AdityA4701@users.noreply.github.com>
2026-09-04 19:52:51 +0000

    operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valInnumber = Number(score);// jo bhi type ho usko capitalize kardo to convert to that type
 
-console.log(typeof valInnumber);
-console.log(valInnumber);
+//console.log(typeof valInnumber);
+//console.log(valInnumber);
 
 // "33" => 33
 // "33abc" => NaN (not a number)
@@ -15,8 +15,23 @@ let isLoggedIn = ""
 
 let boolisLoggedIn = Boolean(isLoggedIn)
 
-console.log(boolisLoggedIn);
+//console.log(boolisLoggedIn);
 
 
 // true => 1, false => 0
 // "" => 0, "aditya" => 1
+
+
+
+// ************* Operations **************
+
+//console.log(1 + 2 + "2" + 3 + 6);
+//console.log(+true)
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+//console.log(num1);
+
