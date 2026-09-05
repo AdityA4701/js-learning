b999bf66f126097a32ae2370dad8a1d4eb6b0c02
Aditya Ranjan <63051958+AdityA4701@users.noreply.github.com>
2026-09-05 05:20:53 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,39 @@
+const name = "Aditya"
+const repocount = 50
+
+// console.log(name + repocount + " hey");
+
+// console.log(`My name is ${name} and i have ${repocount} repos in my github`)
+
+const gameName = new String('Aditya-fc-com')
+
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('d'))
+
+
+const newString = gameName.substring(0,3)
+
+// console.log(newString);
+
+const anotherstring = gameName.slice(-7,5)
+
+// console.log(anotherstring);
+
+
+const url = "https://aditya.com/aditya%20ranjan"
+
+// console.log(url.replace('%20' , '-'))
+
+// console.log(gameName.split('-'));
+
+const words = gameName.split('-')
+console.log(words[1])
+
+
+
+
