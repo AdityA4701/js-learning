da1e3a3332cae49c188f48ec2ffb57db572d584a
Aditya Ranjan <63051958+AdityA4701@users.noreply.github.com>
2026-09-05 15:56:00 +0000

    math and num

---
+++ 01_basics/06_numbers_maths.js
@@ -0,0 +1,32 @@
+const num = 33
+const number = new Number(46)
+
+// console.log(number.toFixed(1))
+
+// console.log(num);
+// console.log(number);
+
+const price = 39.3993
+
+// console.log(price.toPrecision(3))
+
+// console.log(number.toString().length)
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// +++++++++++++++++++++++++++++ maths +++++++++
+
+// console.log(Math)
+// console.log(Math.round(4.6))
+// console.log(Math.random())
+
+console.log(Math.floor((Math.random() * 10) + 1))
+
+
+const max = 20
+const min = 10
+console.log(Math.floor((Math.random() * (max - min + 1)) + min))
+
